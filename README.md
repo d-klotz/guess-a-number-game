61fb8e0d49073d41ec6de37089b8316926256d46
Daniel Klotz <dklotz@goc.realcore-it.de>
2019-10-12 10:28:58 +0200

    Added Input styling, validation and state management

---
+++ README.md
@@ -0,0 +1,11 @@
+# Guess a number App made with React Native
+
+## Running the App
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:19002](http://localhost:19002) to view Expo page, then scan the QR Code with the Expo App ([Android](https://play.google.com/store/apps/details?id=host.exp.exponent&hl=pt), [iOS](https://apps.apple.com/br/app/expo-client/id982107779)).
+
+The app will reload if you make edits.<br>
+You will also see any lint errors in the console.
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,22 +1,72 @@
-import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  View, 
+  Text, 
+  Button, 
+  StyleSheet, 
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert
+} from 'react-native';
 
 import Card from '../components/Card';
+import Input from '../components/Input';
 import Colors from '../constants/colors';
 
 const StartGameScreen = () => {
+
+  const [enteredValue, SetEnteredValue] = useState('');
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
+
+  const numberInputHandler = inputText => {
+    SetEnteredValue(inputText.replace(/[^0-9]/g, ''));
+  };
+
+  const resetInputHandler = () => {
+    SetEnteredValue('');
+    setConfirmed(false);
+  }
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredValue);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert('Invalid number!', 
+        'The number has to be between 1 and 99.', 
+        [{text: 'Okay', style:'destructive', onPress: resetInputHandler}])
+      return;
+    }
+    setConfirmed(true);
+    setSelectedNumber(chosenNumber);
+    SetEnteredValue('');
+  }
+
+  let confirmedOutput;
+
+  if (confirmed) {
+    confirmedOutput = <Text>Chosen number: {selectedNumber}</Text>
+  }
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a new game!</Text>
-      <Card style={styles.inputContainer}>
-        <Text>Select a number</Text>
-        <TextInput />
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}><Button title="Reset" color={Colors.secondary} onPress={() => {}}/></View>
-          <View style={styles.button}><Button title="Confirm" color={Colors.primary} onPress={() => {}}/></View>
-        </View>
-      </Card>
-    </View>
+    <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a new game!</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a number</Text>
+          <Input 
+            style={styles.input} 
+            keyboardType="number-pad" 
+            maxLength={2}
+            onChangeText={numberInputHandler}
+            value={enteredValue}/>
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}><Button title="Reset" color={Colors.secondary} onPress={resetInputHandler}/></View>
+            <View style={styles.button}><Button title="Confirm" color={Colors.primary} onPress={confirmInputHandler}/></View>
+          </View>
+        </Card>
+        {confirmedOutput}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -43,6 +93,10 @@ const styles = StyleSheet.create({
   },
   button: {
     width: 90
+  },
+  input: {
+    width: 50,
+    textAlign: 'center'
   }
 });
 
